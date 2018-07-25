cc9b8c791a16de61e7e767c3d9786361d769423b
Nesar Ahmed Nishad <rockstar.nishad@gmail.com>
2018-07-25 15:10:01 +0600

    Nishad

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <h1>Simple E-commerce Project</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
---
+++ src/components/pages/login.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="login">
+    <div class="loginHeader" align="center">LOGIN</div>
+    <div class="loginContainer">
+      <table>
+        <tr>
+          <td>Username</td>
+          <td>:</td>
+          <td><input type="text" placeholder="username...." v-model="user.username"></td>
+        </tr>
+        <tr>
+          <td>Password</td>
+          <td>:</td>
+          <td><input type="password" placeholder="password...." v-model="user.username"></td>
+        </tr>
+        <tr>
+          <td colspan="3" align="center"><button class="addBtn" @click="loginNow()">Log Me In</button></td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      user: {
+        username: "",
+        password:""
+      }
+    }
+  },
+  methods: {
+    loginNow(){
+      // console.log(this.user)
+      this.$eventBus.$emit("loadingStatus", true);
+    }
+  }
+}
+</script>
+
+
